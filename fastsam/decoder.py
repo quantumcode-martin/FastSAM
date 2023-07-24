9350eab0fc5f4b2844f4a96b13d75c2bdec4714c
QuantumMartin <31007551+quantumcode-martin@users.noreply.github.com>
2023-07-24 11:01:56 +0200

    Fixed decoder CASIA-IVA-Lab#111
    I fixed the error 'FastSAMDecoder' object has no attribute 'results' by replacing results with image_embeddings.
    Also replaced self.ori_img by self.image as the class had no ori_image as well.
    
    Now this is running  fine for me.

--- fastsam/decoder.py
+++ fastsam/decoder.py
@@ -9,12 +9,12 @@ class FastSAMDecoder:
     def __init__(
         self,
         model: FastSAM,
-        device: str='cpu',
-        conf: float=0.4, 
-        iou: float=0.9,
-        imgsz: int=1024,
-        retina_masks: bool=True,
-        ):
+        device: str = "cpu",
+        conf: float = 0.4,
+        iou: float = 0.9,
+        imgsz: int = 1024,
+        retina_masks: bool = True,
+    ):
         self.model = model
         self.device = device
         self.retina_masks = retina_masks
@@ -23,29 +23,29 @@ class FastSAMDecoder:
         self.iou = iou
         self.image = None
         self.image_embedding = None
-        
+
     def run_encoder(self, image):
-        if isinstance(image,str):
-            image =  np.array(Image.open(image))
+        if isinstance(image, str):
+            image = np.array(Image.open(image))
         self.image = image
         image_embedding = self.model(
-            self.image, 
-            device=self.device, 
-            retina_masks=self.retina_masks, 
-            imgsz=self.imgsz, 
-            conf=self.conf, 
-            iou=self.iou
-            )
+            self.image,
+            device=self.device,
+            retina_masks=self.retina_masks,
+            imgsz=self.imgsz,
+            conf=self.conf,
+            iou=self.iou,
+        )
         return image_embedding[0].numpy()
 
     def run_decoder(
-            self,
-            image_embedding, 
-            point_prompt: Optional[np.ndarray]=None,
-            point_label: Optional[np.ndarray]=None,
-            box_prompt: Optional[np.ndarray]=None,
-            text_prompt: Optional[str]=None,
-            )->np.ndarray:
+        self,
+        image_embedding,
+        point_prompt: Optional[np.ndarray] = None,
+        point_label: Optional[np.ndarray] = None,
+        box_prompt: Optional[np.ndarray] = None,
+        text_prompt: Optional[str] = None,
+    ) -> np.ndarray:
         self.image_embedding = image_embedding
         if point_prompt is not None:
             ann = self.point_prompt(points=point_prompt, pointlabel=point_label)
@@ -60,10 +60,10 @@ class FastSAMDecoder:
             return None
 
     def box_prompt(self, bbox):
-        assert (bbox[2] != 0 and bbox[3] != 0)
+        assert bbox[2] != 0 and bbox[3] != 0
         masks = self.image_embedding.masks.data
-        target_height = self.ori_img.shape[0]
-        target_width = self.ori_img.shape[1]
+        target_height = self.image.shape[0]
+        target_width = self.image.shape[1]
         h = masks.shape[1]
         w = masks.shape[2]
         if h != target_height or w != target_width:
@@ -71,7 +71,8 @@ class FastSAMDecoder:
                 int(bbox[0] * w / target_width),
                 int(bbox[1] * h / target_height),
                 int(bbox[2] * w / target_width),
-                int(bbox[3] * h / target_height), ]
+                int(bbox[3] * h / target_height),
+            ]
         bbox[0] = round(bbox[0]) if round(bbox[0]) > 0 else 0
         bbox[1] = round(bbox[1]) if round(bbox[1]) > 0 else 0
         bbox[2] = round(bbox[2]) if round(bbox[2]) < w else w
@@ -80,7 +81,7 @@ class FastSAMDecoder:
         # IoUs = torch.zeros(len(masks), dtype=torch.float32)
         bbox_area = (bbox[3] - bbox[1]) * (bbox[2] - bbox[0])
 
-        masks_area = np.sum(masks[:, bbox[1]:bbox[3], bbox[0]:bbox[2]], axis=(1, 2))
+        masks_area = np.sum(masks[:, bbox[1] : bbox[3], bbox[0] : bbox[2]], axis=(1, 2))
         orig_masks_area = np.sum(masks, axis=(1, 2))
 
         union = bbox_area + orig_masks_area - masks_area
@@ -89,20 +90,22 @@ class FastSAMDecoder:
 
         return np.array([masks[max_iou_index].cpu().numpy()])
 
-    def point_prompt(self, points, pointlabel):  # numpy 
-
-        masks = self._format_results(self.results[0], 0)
-        target_height = self.ori_img.shape[0]
-        target_width = self.ori_img.shape[1]
-        h = masks[0]['segmentation'].shape[0]
-        w = masks[0]['segmentation'].shape[1]
+    def point_prompt(self, points, pointlabel):  # numpy
+        masks = self._format_results(self.image_embedding[0], 0)
+        target_height = self.image.shape[0]
+        target_width = self.image.shape[1]
+        h = masks[0]["segmentation"].shape[0]
+        w = masks[0]["segmentation"].shape[1]
         if h != target_height or w != target_width:
-            points = [[int(point[0] * w / target_width), int(point[1] * h / target_height)] for point in points]
+            points = [
+                [int(point[0] * w / target_width), int(point[1] * h / target_height)]
+                for point in points
+            ]
         onemask = np.zeros((h, w))
-        masks = sorted(masks, key=lambda x: x['area'], reverse=True)
+        masks = sorted(masks, key=lambda x: x["area"], reverse=True)
         for i, annotation in enumerate(masks):
             if type(annotation) == dict:
-                mask = annotation['segmentation']
+                mask = annotation["segmentation"]
             else:
                 mask = annotation
             for i, point in enumerate(points):
@@ -112,7 +115,7 @@ class FastSAMDecoder:
                     onemask[mask] = 0
         onemask = onemask >= 1
         return np.array([onemask])
-    
+
     def _format_results(self, result, filter=0):
         annotations = []
         n = len(result.masks.data)
@@ -122,10 +125,10 @@ class FastSAMDecoder:
 
             if np.sum(mask) < filter:
                 continue
-            annotation['id'] = i
-            annotation['segmentation'] = mask
-            annotation['bbox'] = result.boxes.data[i]
-            annotation['score'] = result.boxes.conf[i]
-            annotation['area'] = annotation['segmentation'].sum()
+            annotation["id"] = i
+            annotation["segmentation"] = mask
+            annotation["bbox"] = result.boxes.data[i]
+            annotation["score"] = result.boxes.conf[i]
+            annotation["area"] = annotation["segmentation"].sum()
             annotations.append(annotation)
         return annotations
